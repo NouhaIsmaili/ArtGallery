9e060a5020d8a813c91ff2a5b824afb6ddb73845
=islem <=islemomrani1005@gmail.com>
2024-11-22 22:47:14 +0100

    ajout pipe personalisée

--- src/app/components/star.pipe.ts
+++ src/app/components/star.pipe.ts
@@ -6,11 +6,8 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class StarPipe implements PipeTransform {
   transform(productName: string, likes: number): string {
-    // Calculer le nombre d'étoiles (maximum 5 étoiles)
-    const starsCount = Math.min(Math.floor(likes / 100), 5);
-    // Générer les étoiles
+    const starsCount = Math.min(Math.floor(likes/100), 5);
     const stars = '⭐'.repeat(starsCount);
-    // Retourner le nom du produit avec les étoiles
     return `${productName} ${stars}`;
   }
 }
